19ff4da7bdebc537ace2d368c414ca5b4ed30680
Xue Jiao <jiao.xuejiao@gmail.com>
2020-04-11 10:28:43 +0800

    Add tests for close method

--- raw_linux_test.go
+++ raw_linux_test.go
@@ -455,6 +455,33 @@ func Test_packetConnWriteToSendtoOK(t *testing.T) {
     }
 }
 
+// Test that socket close functions as intended
+
+type captureCloseSocket struct {
+    closed bool
+    noopSocket
+}
+
+func (s *captureCloseSocket) Close() error {
+    s.closed = true
+    return nil
+}
+
+func Test_packetConnClose(t *testing.T) {
+    s := &captureCloseSocket{}
+    p := &packetConn{
+        s: s,
+    }
+
+    if err := p.Close(); err != nil {
+        t.Fatal(err)
+    }
+
+    if !s.closed {
+        t.Fatalf("socket should be closed, but is not")
+    }
+}
+
 // testSleeper is a sleeper implementation which atomically increments a
 // couter to indicate how long it has slept
 type testSleeper struct {
